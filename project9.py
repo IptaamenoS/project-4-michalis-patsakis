88873c52e7dd9944f57aece1b4ee3b14f6f0e2a6
IptaamenoS <60153799+IptaamenoS@users.noreply.github.com>
2020-02-26 15:32:16 +0200

    Create project9.py

---
+++ project9.py
@@ -0,0 +1,16 @@
+Print("Δώσε μου αριθμό")\
+i=0
+n=int(input())
+while (n>9):
+  n*=3
+  n+=1
+  k=n
+  count=0
+  
+while(k>0):
+    count=count+1
+    n=n//10
+    for x inrange(count)
+    i= i + (n//10)
+
+     
