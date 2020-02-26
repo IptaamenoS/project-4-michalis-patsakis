2d504a696b60940748c64d73796c962bfbab9217
IptaamenoS <60153799+IptaamenoS@users.noreply.github.com>
2020-02-26 22:09:27 +0200

    Update project4.py

--- project4.py
+++ project4.py
@@ -3,14 +3,15 @@ def AscII_trans( text):
  number= [ord(n) for n in text]
  l = len(number)
  for i in range(l):
-  k= int(number[i])
-  if (k <= 3 and k >1) :
-   print("thats a  prime a number")
-  if ( k % 2 == 0 or k % 3 == 0 ):
-   print("thats not a prime number")
-  i = 5
-  while(i * i <= k) : 
-   if (k % i == 0 or k % (i + 2) == 0) : 
-    print ("thats not a prime number")
-    i = i + 6
+  e= e +number[i]
+ k= int(e)
+ if (k <= 3 and k >1) :
+  print("thats a  prime a number")
+ if ( k % 2 == 0 or k % 3 == 0 ):
+  print("thats not a prime number")
+ i = 5
+ while(i * i <= k) : 
+  if (k % i == 0 or k % (i + 2) == 0) : 
+   print ("thats not a prime number")
+   i = i + 6
 
