7a2e53a34a6710702848dc653b515d9101361835
IptaamenoS <60153799+IptaamenoS@users.noreply.github.com>
2020-02-26 12:53:42 +0200

    Update project4.py

--- project4.py
+++ project4.py
@@ -1,11 +1,10 @@
 def AscII_trans( text):
  global number 
  number= [ord(n) for n in text]
+ for i in number:
+  If( i > 0 and i < 8 and i != 4 and i != 6)
+  Print(i + ' είναι πρώτος αριθμός')
+ else 
+  Print( i + "δεν είναι πρώτος αριθμός")
 Print(" Δώστε λέξη")
 text=input()
-AscII_trans(text)
-for i in number:
-  If( i > 0 and i < 8 and i != 4 and i != 6)
-    Print(i + ' είναι πρώτος αριθμός')
-  else 
-    Print ( i + "δεν είναι πρώτος αριθμός")
