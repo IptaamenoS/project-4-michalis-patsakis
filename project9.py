97b0c439173482c590ec82b4fe82cb4a347412ca
IptaamenoS <60153799+IptaamenoS@users.noreply.github.com>
2020-02-26 20:28:30 +0200

    Update project9.py

--- project9.py
+++ project9.py
@@ -1,16 +1,18 @@
-Print("Δώσε μου αριθμό")\
-i=0
+Print("Δώσε μου αριθμό")
 n=int(input())
 while (n>9):
   n*=3
   n+=1
   k=n
   count=0
-  
-while(k>0):
+  while(k>0):
     count=count+1
-    n=n//10
-    for x inrange(count)
-    i= i + (n//10)
+    k=k//10
+  y=0
+  for x inrange(count)
+    k= n%10
+    n= n//10
+    y= y + k
+  n=y
 
      
